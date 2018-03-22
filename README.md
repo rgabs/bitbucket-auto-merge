5b924458469120354321c1c1e7b466b5cab90ba3
Rahul gaba <rahulgaba16@gmail.com>
2018-03-22 11:17:48 +0700

    Packaging stuff

---
+++ README.md
@@ -0,0 +1,8 @@
+Presenting you a chrome extension for automating the 4 clicks required for each PR merge.
+Steps to install:
+1. Visit chrome://extensions/
+2. Enable dev mode from top right
+3. Drag and drop extenstion(ext.crx file)
+4. Click on Add extension button
+
+That’s all, now open any PR and you will see `Approve, squash and merge` button on top right after the page is loaded. Click on the button to approve, squash and merge the PR.
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -17,19 +17,21 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		const styles = "position: fixed; z-index: 1000000; right: 20px;background-color: #0547A5; color: white;padding: 10px; border-radius: 2px; border-color: transparent; ";
 		
-		const approveMergeNode = '<button id="approve-merge" style="' + styles +'">Approve and merge</button>'
+		const approveMergeNode = '<button id="approve-merge" style="' + styles +'">Approve, Squash and merge</button>'
 		$('body').prepend(approveMergeNode);
 		const approveMergeBtn = $('#approve-merge');
 		approveMergeBtn.click(() => execScripts(500, [
 			"$('.approve-button').click();", //approve
 			"$('#fulfill-pullrequest').click()", // start merge process
 			"$('#id_merge_strategy').val('squash')", // squash
+			"$('.select2-chosen').html('Squash')", // setting text to squash
 			"$('#id_close_anchor_branch').prop('checked', true)", // check delete checkbox
 			"$('button.button-panel-button').click()" //merge button
 		]));
 
 		const developAlphaNode = '<button id="alpha-develop-merge" style="' + styles + ' right: 175px; ">develop -> alpha merge</button>'
-		$('body').prepend(developAlphaNode);
+
+		// $('body').prepend(developAlphaNode); this will enable develop -> alpha merge button (not tested)
 		const developAlphaBtn = $('#alpha-develop-merge');
 		developAlphaBtn.click(() => execScripts(500, [
 			"$('.approve-button').click();",
